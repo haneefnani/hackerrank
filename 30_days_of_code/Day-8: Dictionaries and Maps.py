87269f7c3d98c7a33fdd1199370a554c651c8f38
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-04-12 22:53:24 +0530

    Update Day-8: Dictionaries and Maps.py

--- 30_days_of_code/Day-8: Dictionaries and Maps.py
+++ 30_days_of_code/Day-8: Dictionaries and Maps.py
@@ -17,3 +17,30 @@ for j in range(0, num):
         print(name + "=" + str(phone))
     else:
         print("Not found")
+
+'''
+##Other Way##
+
+import sys
+inputList=[]
+
+for line in sys.stdin:
+    inputList.append(line)
+
+n = int(inputList[0])
+entries = inputList[1:n+1]
+queries = inputList[n+1:]
+
+phoneBook = {}
+
+for entry in entries:
+    name, id = entry.split()
+    phoneBook[name] = id
+
+for query in queries:
+    stripQuery = query.rstrip() #Eliminates the newline character
+    if stripQuery in phoneBook:
+        print(stripQuery + "=" + str(phoneBook[stripQuery]))
+    else:
+        print("Not found")
+        '''
