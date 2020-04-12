0dccdbe0b119cedee3615a5af940edfa09a26bc7
haneefnani <55850551+haneefnani@users.noreply.github.com>
2020-04-12 23:07:31 +0530

    Update Poisson Distribution I.py

--- 10-Days-of-Statistics/Day-5/Poisson Distribution I.py
+++ 10-Days-of-Statistics/Day-5/Poisson Distribution I.py
@@ -1,8 +1,18 @@
-import math
-def poisson(k, lam):
-    return (math.pow(lam, k) * math.pow(math.e, -1*lam)) / math.factorial(k)
+### Question: A random variable, X, follows Poisson distribution with mean of 2.5
+### Find the probability with which the random variable X is equal to 5.
 
-lam = input()
-k = input()
+# Define functions
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    if n > 1:
+        return factorial(n - 1) * n
 
-print poisson(k, lam)
+# Set data
+mean = float(input())
+k = float(input())
+e = 2.71828
+
+# Gets the result and show on the screen
+result = ((mean ** k) * (e ** -mean)) /  factorial(k)
+print(round(result, 3))
